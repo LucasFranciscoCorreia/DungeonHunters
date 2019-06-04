14ed1ab4f9996017b1b511b6165914eefcce7f63
Pedro Antônio de Souza Silva <pedroantoniodesouzasilva@gmail.com>
2019-06-04 00:04:36 -0300

    Sound ++
    Enemy Death sound + Attack ++

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -16,6 +16,7 @@ public class EnemyHealth : MonoBehaviour
     {
         if(health <= 0)
         {
+            FindObjectOfType<AudioManager>().Play("inimigoMorreu");
             Destroy(gameObject);
         }
     }
