785813dbefee9bcdf855f943a7f46d7971897592
Igor <igor11souza@gmail.com>
2019-06-29 17:41:16 -0300

    barra de stamina e personagem correndo

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Slider sliderStamina;
+
+    public void SetStamina(float maxStamina, float currentStamina)
+    {
+        float newPosition = currentStamina / maxStamina;
+
+        sliderStamina.value = newPosition;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
