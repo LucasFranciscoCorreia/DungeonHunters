3e268c9d6070a61113ac4117e89a4b9feae59882
lucas correia <lfpgc96@hotmail.com>
2019-06-29 22:19:01 -0300

    correções merge

--- Assets/Scripts/hero_script.cs
+++ Assets/Scripts/hero_script.cs
@@ -15,8 +15,6 @@ public class hero_script : MonoBehaviour
 
     public Animator weaponAnimator;
     private float speed;
-    public float speedWalk;
-    public float speedRun;
     public float startTimeAttack;
     public Transform attackPos;
     public float attackRangeX, attackRangeY;
@@ -34,7 +32,7 @@ public class hero_script : MonoBehaviour
     void Start()
     {
         instance = this;
-        //speed = 2.5f;
+        speed = 2.5f;
         animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
         rightTurned = true;
@@ -53,20 +51,21 @@ public class hero_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //UI.SetStamina(maxStamina, currentStamina);
         if (!PauseMenu.isPaused)
         {
+            UI.SetStamina(maxStamina, currentStamina);
             var isWalking = false;
             var isAbleAttack = true;
-            var isRunning = false;
-            var value = 1f;
             float hori = 0, vert = 0;
-            if (Input.GetKey(KeyCode.RightShift) && currentStamina > 0)
+            var value = 1f;
+            if (Input.GetKey(KeyCode.LeftShift) && currentStamina > 0)
             {
-                isRunning = true;
-                speed = speedRun;
                 value = 1.5f;
-                currentStamina -= runCostStamina;
+                currentStamina -= 2*Time.deltaTime;
+            }
+            else if (currentStamina < maxStamina)
+            {
+                currentStamina += Time.deltaTime;
             }
             if (currentStamina < 0)
             {
@@ -74,29 +73,27 @@ public class hero_script : MonoBehaviour
             }
             if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
             {
-                if (!isRunning)
-                {
-                    isWalking = true;
-                    isRunning = false;
-                    speed = speedWalk;
-                }
+
+                isWalking = true;
+
                 if (Input.GetKey(KeyCode.A))
                 {
-                    hori = -value;
+                    hori = -1;
                 }
                 else if (Input.GetKey(KeyCode.D))
                 {
-                    hori = value;
+                    hori = 1;
                 }
                 if (Input.GetKey(KeyCode.W))
                 {
-                    vert = value;
+                    vert = 1;
                 }
                 else if (Input.GetKey(KeyCode.S))
                 {
-                    vert = -value;
+                    vert = -1;
                 }
-                body.MovePosition(new Vector2((transform.position.x + hori * speed * Time.deltaTime), (transform.position.y + vert * speed * Time.deltaTime)));
+                Debug.Log(speed);
+                body.MovePosition(new Vector2((transform.position.x + hori * speed * value * Time.deltaTime), (transform.position.y + vert * speed * value * Time.deltaTime)));
                 Vector3 scale = transform.localScale;
                 if (hori > 0 && !rightTurned)
                 {
@@ -106,28 +103,28 @@ public class hero_script : MonoBehaviour
                 }
                 else if (hori < 0 && rightTurned)
                 {
-                    
+
                     rightTurned = false;
                     scale.x = -1;
                     transform.localScale = scale;
                 }
             }
-            if(timeAttack <= 0)
+            if (timeAttack <= 0)
             {
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     timeAttack = startTimeAttack;
                     isAbleAttack = false;
                     FindObjectOfType<AudioManager>().Play("porrada");
-                    Collider2D[] enemies = Physics2D.OverlapBoxAll(attackPos.position, new Vector2(attackRangeX, attackRangeY),0, enemieLayer);
-                    for(int i = 0; i < enemies.Length; i++)
+                    Collider2D[] enemies = Physics2D.OverlapBoxAll(attackPos.position, new Vector2(attackRangeX, attackRangeY), 0, enemieLayer);
+                    for (int i = 0; i < enemies.Length; i++)
                     {
                         enemies[i].GetComponent<EnemyHealth>().TakeDamage(damage);
                     }
                 }
                 else
                 {
-                   
+
                     isAbleAttack = true;
                 }
             }
